59de79e4c64f2a333399f27a419928df86dcacb5
Soung jin Ryu <rsj11201@gmail.com>
2020-02-26 10:14:05 +0900

    Update effect.js

--- v.2/js/effect.js
+++ v.2/js/effect.js
@@ -9,14 +9,15 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-158025067-2');
 $.fn.effect = function(options) {
-var documentHeight =document.documentElement.clientHeight,
-    documentWidth = document.documentElement.clientWidth,
-    randArray=(a)=>{return a[Math.floor(Math.random() * a.length)]},
+  var randArray=(a)=>{return a[Math.floor(Math.random() * a.length)]},
     defaults = {
         minSize: 10,maxSize: 50,
+        documentWidth:()=>{return document.documentElement.clientWidth},
+        documentHeight:()=>{return document.documentElement.clientHeight},
         target:"body",
         text:'&#10052',
         direction:"top",
+        transform:"",
         color: "#fff",
         time:300,anitime:1000,glitter:false,//애니메이션
         startOpacity:1,endOpacity:.2,
@@ -26,6 +27,8 @@ var documentHeight =document.documentElement.clientHeight,
     },isPlay = true,
     options=$.extend({},defaults,options),
     $flake=$('<div id="flake" />').css({'position':'absolute',color:options.color}).html(options.text);
+    console.log(options.transform);
+    $(options.target).css({"transform":options.transform})
     if(options.glitter)//사용자 애니메이션
       $flake.css("animation",options.glitter+" "+options.anitime+"s infinite")
     for(var i=0;i<options.count;i++){
@@ -40,19 +43,21 @@ var documentHeight =document.documentElement.clientHeight,
             if(["top","bottom"].indexOf(d["move_direction"])!=-1)r="left";
             if(!d.func)
               d.func=function(d,r,s,e){
-                var wp=Math.random()*documentWidth;
-                s[r]=Math.random()*documentHeight;s[d["move_direction"]]=wp;
+                var wp=Math.random()*options.documentWidth();
+                s[r]=Math.random()*options.documentHeight();s[d["move_direction"]]=wp;
                 if(d.direction!="none")
                   e[d["move_direction"]]=wp+(options.move*Boolean(Math.round(Math.random())));
               };
           }else if(["top","bottom"].indexOf(d.direction)!=-1)r="left";
           else if(["left","right"].indexOf(d.direction)!=-1)r="top";
-          s[d.direction]=s.pos;e[d.direction]=e.pos;
-		  console.log(s[d.direction]);
+          s[d.direction]=s.pos;
+          if(d.direction=="top")
+            e[d.direction]=e.pos;
+          else e[d.direction]=options.documentWidth()+10;
           if(!d.func)
             d.func=function(d,r,s,e){
-              s[r]=(Math.random()*documentWidth*(d.move==200?1:1.5))+(d.move==200?0:d.move);
-              e[r]=s[r]-100+Math.random()*d.move;
+              s[r]=(Math.random()*options.documentWidth())+(d.move==200?0:d.move);
+              e[r]=s[r]+Math.random()*d.move;
             };
           if(r)d.func(d,r,s,e);
           f=()=>{
@@ -60,18 +65,14 @@ var documentHeight =document.documentElement.clientHeight,
             d.loop?
               o.css(s).
               animate({opacity:options.startOpacity},d.time,d.anitiming).delay(Math.random()*d.delay).
-              animate(e,documentHeight*d.speed+Math.random() * 1000,d.anitiming).
+              animate(e,options.documentHeight()*d.speed+Math.random() * 1000,d.anitiming).
               animate({opacity:options.endOpacity},d.time,d.anitiming,f):0;
             return function(){isplay=false;$(o).remove()}};
           return f;
       })(obj,options,{
           pos:-1*(options.maxSize+50),opacity:0,
-      },{pos:documentHeight+10})();
+      },{pos:options.documentHeight()+10})();
     }
-$(window).resize(function (){
-  documentHeight=document.documentElement.clientHeight;
-  documentWidth=document.documentElement.clientWidth;
-});
-return function(){isPlay=false};
+  return function(){isPlay=false};
 };
 })(jQuery);
